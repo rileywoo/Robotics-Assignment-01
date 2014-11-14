0ee5414b52ffc448d4931a2594ef811de47b5e05
Riley Woo <mylifeisriley@rileys-mbp.attlocal.net>
2014-11-13 22:25:20 -0800

    First commit

--- roboticsAssignment1part1/main.cpp
+++ roboticsAssignment1part1/main.cpp
@@ -5,11 +5,12 @@
 //  Created by Riley Woo on 11/13/14.
 //  Copyright (c) 2014 Riley Woo. All rights reserved.
 //
+//  Outputs name
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
+    std::cout << "My life is Riley." << std::endl;
     return 0;
 }
